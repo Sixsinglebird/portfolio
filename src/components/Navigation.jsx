bbd023dc9e1ab52417cb93452770ae0d1d140e67
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-10-30 17:20:53 -0230

    Update Navigation.js

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,12 @@ import Button from "./Button";
 const Navigation = () => {
   return (
     <div id="nav" className="flex">
-      <a href="https://www.dropbox.com/scl/fi/2tkbfcmfkm611iibt1jde/Resume-2023_10_06.pdf?rlkey=b4xfb8ilezpqdeecftokg51gn&dl=1">
+      <a href="https://www.dropbox.com/scl/fi/ts32m1ff4m3uotfj05uyb/Resume-2023_10_30.pdf?rlkey=qj5gpoa1de3x4d96ztawv058x&dl=1">
         <Button text="My Resume" />
+      </a>
+      <a href="https://modrinth.com/mod/wizardfabric">
+        {" "}
+        <Button text="WizardFabric" />
       </a>
     </div>
   );
